8e3c9feeebfc4f7e87801c0738f7ea1604dd0d48
brenda oduor <bresharchie2@gmail.com>
2022-09-22 20:50:35 +0300

    python import and modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,6 @@ if __name__ == '__main__':
     import calculator_1.add.sub.mul.div
     a = 10
     b = 5
-    
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, add(a, b)))
     print("{} * {} = {}".format(a, b, add(a, b)))
