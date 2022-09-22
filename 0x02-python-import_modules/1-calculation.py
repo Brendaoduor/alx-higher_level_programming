65e89396a1cbc9dcd19dcc034cc68c7a19d1e491
brenda oduor <bresharchie2@gmail.com>
2022-09-22 21:00:48 +0300

    python import and modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     """this program adds, subtracts, divides and multiplies two numbers"""
-    import calculator_1.add.sub.mul.div
+    from calculator_1 import add
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
+    
+    from . import sub
     print("{} - {} = {}".format(a, b, add(a, b)))
+
+    from . import mul
     print("{} * {} = {}".format(a, b, add(a, b)))
+
+    from . import div
     print("{} / {} = {}".format(a, b, add(a, b)))
