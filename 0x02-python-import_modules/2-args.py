be540140fa648237b02470dbfbe0eead9f5776bc
brenda oduor <bresharchie2@gmail.com>
2022-09-24 19:37:16 +0300

    python import and modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,8 @@ import sys
 if __name__ == "__main__":
     """prints command line arguments"""
 
-    index = len(sys.argv) - 1
-    if index == 0:
+    index = len(sys.argv)
+    if sys.argv[0]:
         print(index, "arguments.")
     elif index == 1:
-        print(index, "arguments:")
-    else:
         print("{} arguments:".format(index))
-    for indx in range(index):
-        print("{} : {}".format(indx + 1, sys.argv[indx + 1]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import hidden_4
-folders = dir(hidden_4) 
-if hidden_4.path.islist(name) and name[0] != '_':
-    print(dir(folders))
+folders = dir(hidden_4)
+for index[0] in folders != '__':
+    print(folders)
