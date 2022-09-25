46f70f967b615407f21435234ddc95e600f595ad
brenda oduor <bresharchie2@gmail.com>
2022-09-25 21:35:47 +0300

    python data structures

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for i in range(len(my_list)):
+        if idx == [-idx]:
+            pass
+        elif idx > len(my_list):
+            pass
+        else:
+            return(my_list[idx])
