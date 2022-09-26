78bf0952279632c1403d9a909cec53d110c787eb
brenda oduor <bresharchie2@gmail.com>
2022-09-26 14:41:14 +0300

    python data structures

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,11 +5,3 @@ def delete_at(my_list=[], idx=0):
         else:
          my_list.remove(idx + 1)
          return my_list
-
-delete_at = __import__('11-delete_at').delete_at
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
