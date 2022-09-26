4ab62de95fda3c2f76aac781141cf67c4ab0974d
brenda oduor <bresharchie2@gmail.com>
2022-09-27 01:36:14 +0300

    python data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    for i in range(len(sentence)):
-        if sentence == "":
-            return (len(sentence), None)
-        else:
-            return(len(sentence), sentence[0])
+    if sentence == "":
+        return (len(sentence), None)
+    else:
+        return(len(sentence), sentence[0])
