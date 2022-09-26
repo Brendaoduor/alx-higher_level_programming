5f38f022801867934def0b75cf5be81f5862235b
brenda oduor <bresharchie2@gmail.com>
2022-09-26 11:20:23 +0300

    python data structures

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,10 +2,3 @@
 def no_c(my_string):
     result ="".join([i for i in my_string if i != 'c' and i != 'C'])
     return (result)
-
-no_c = __import__('5-no_c').no_c
-
-print(no_c("Best School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
-
