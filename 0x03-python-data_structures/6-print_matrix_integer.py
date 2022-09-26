8c9d37e481dd8ac1a18e398e344cedf09b9f8b76
brenda oduor <bresharchie2@gmail.com>
2022-09-27 02:42:55 +0300

    python data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,24 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-       """matrix = [[column * row for column in range(3)] for row in range(3)]
-          matrix[column][row] = column
-          print(matrix[column][row], end=" ")"""    
-n = 3
-m = 3
-matrix = [0] * n
-for x in range(n):
-    matrix[x] = [0] * m
-print(matrix)
-
-
-print_matrix_integer = __import__('6-print_matrix_integer').print_matrix_integer
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
+    matrix = [[column * row for column in range(len(matrix))] for 
+            row in range(len(matrix[column))]
+    print("{:d}".formt(matrix[column][row]), end="")
+    if(column != (len(matrix[i] - 1):
+        print(" ", end="")
