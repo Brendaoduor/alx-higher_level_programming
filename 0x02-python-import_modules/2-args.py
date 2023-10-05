a497b087147fe8dc3c964d5aa8b3baf960d9b043
brendaoduor <bresharchie2@gmail.com>
2023-10-06 00:04:58 +0300

    import and modules task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,17 @@ import sys
 if __name__ == "__main__":
     """prints command line arguments"""
 
-    index = len(sys.argv)
-    if sys.argv[0]:
+    index = len(sys.argv) - 1
+    if index == 0:
         print(index, "arguments.")
     elif index == 1:
-        print("{} arguments:".format(index))
+        print("{} argument:". format(index))
+    else:
+        print("{} arguments:". format(index))
+
+    if index >= 1:
+        index = 0
+        for arg in sys.argv:
+            if index != 0:
+                print("{}: {}". format(index, arg))
+            index += 1
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-folders = dir(hidden_4)
-for index[0] in folders != '__':
-    print(folders)
+if __name__ == "__main__":
+    import hidden_4
+    for folders in dir(hidden_4):
+        if folders[0] != '_' and folders[1] != '_':
+            print(folders)
