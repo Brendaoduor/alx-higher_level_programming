9b6d98180198e6cccc457b1cccd775e706e3a4a4
brendaoduor <bresharchie2@gmail.com>
2024-08-23 15:04:02 +0300

    classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""class square that defines a square"""
+
+    def __init__(self, size = 0):
+        """This initializes the square
+
+        Args:
+            value(int): size of the square.
+        """
+        self.size = size
+
+    def size(self):
+        """retrieves the value
+
+        Returns:
+            private size
+        """
+        return self.__size
+
+    def size(self, value):
+        """sets the size value
+
+        Args:
+            value (int): size of the square.
+        """
+    
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value #: size of the square
+
+    def area(self):
+        """returns the area of the square
+
+        Returns:
+            area.
+        """
+        return self.__size = size ** 2
+
---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3 
+"""class square that defines a square"""
+
+    def __init__(self, size = 0):
+        """This initializes the square
+
+        Args:
+            value(int): size of the square.
+        """
+        self.size = size
+
+    def size(self):
+        """retrieves the value
+
+        Returns:
+            private size
+        """
+        return self.__size
+
+    def size(self, value):
+        """sets the size value
+
+        Args:
+            value (int): size of the square.
+        """
+    
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value #: size of the square
+
+    def area(self):
+        """returns the area of the square
+
+        Returns:
+            area.
+        """
+        return self.__size = size ** 2
+
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
+
+
